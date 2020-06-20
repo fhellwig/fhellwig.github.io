5eef70ff646f54428783bddb5116e9fe741120a9
frank.hellwig <frank.hellwig@hii-tsd.com>
2020-06-20 17:07:03 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export function App() {
+  return (
+    <>
+      <section className="hero is-info">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Frank's Dev Blog</h1>
+            <h2 className="subtitle">Thoughts about software and sofware development</h2>
+          </div>
+        </div>
+      </section>
+      <section className="section">
+        <div className="container">
+          <div className="content">
+            <p>Under construction</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019 Huntington Ingalls Industries, Inc. All rights reserved.
+ */
+
+import 'core-js';
+import 'regenerator-runtime/runtime.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './app.js';
+import './stylesheets/styles.scss';
+
+ReactDOM.render(<App />, document.getElementById('root'));
